ca0566ac58691a9c03a507a093725f0d290e6540
Vladimir <wladimirangelow@gmail.com>
2021-04-12 21:01:00 +0300

    add infinite scroll

--- front-end/src/components/Movies/MoviesByCategory/MoviesByCategory.js
+++ front-end/src/components/Movies/MoviesByCategory/MoviesByCategory.js
@@ -11,32 +11,40 @@ const imageUrl = `http://image.tmdb.org/t/p/w780`
 const MovieByCategory = (props) => {
     const [movies, setMovies] = useState([])
     const [isLoading, setIsLoading] = useState(true)
-    const [counter, setCounter] = useState(1)
     const [user] = useContext(Context)
     const history = useHistory()
-
     const path = props.match.params.category
+    let pageCounter = 3;
 
     useEffect(() => {
-        setCounter(prevState => prevState + 1)
+        Promise.all([
+            getMovies(path, 1),
+            getMovies(path, 2)
+        ]).then(([firstPage, secondPage]) => {
+            setMovies([...firstPage, ...secondPage])
+            setIsLoading(false)
+        })
 
-        getMovies(path, counter)
-            .then(movies => {
-                setMovies(movies)
-                setIsLoading(false)
-            }).catch(err => console.log(err))
+        window.addEventListener("scroll", isScrolling);
+        return () => window.removeEventListener("scroll", isScrolling)
 
     }, [path])
 
-    const loadMore = () => {
-        setCounter(prevState => prevState + 1)
-        return getMovies(path, counter)
+    const loadMore = (pageCounter) => {
+        return getMovies(path, pageCounter)
             .then(movies => {
                 setMovies((prevState) => {
                     return [...prevState, ...movies]
                 })
-                setIsLoading(false)
             }).catch(err => console.log(err))
+            .finally(() => setIsLoading(false))
+    }
+
+    const isScrolling = () => {
+        if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight) {
+            pageCounter++
+            return loadMore(pageCounter)
+        }
     }
 
     if (user.username === '') {
@@ -46,11 +54,7 @@ const MovieByCategory = (props) => {
         }}/>
     }
 
-    if (isLoading) {
-        return (
-            <Loader/>
-        )
-    }
+    if (isLoading) return <Loader/>
 
     const allMovies = movies.map(movie =>
         <span className={styles.categoryMoviePictures} key={movie.id}>
@@ -67,9 +71,6 @@ const MovieByCategory = (props) => {
             <h3 className={styles.title}>{path.toUpperCase()}</h3>
             <div className={styles.allMovies}>
                 {allMovies}
-                <button id={styles['show-more-movies-button']} onClick={loadMore}>
-                    Show More
-                </button>
             </div>
         </Row>
     )
